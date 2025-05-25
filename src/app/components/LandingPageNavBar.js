cdf3834e306220f20fb8c5c5f804bb02564e8c2b
Hadiah Iftikhar <hadiahiftikharntucs@gmail.com>
2025-05-25 10:48:51 +0500

    Still working on the landing page

---
+++ src/app/components/LandingPageNavBar.js
@@ -0,0 +1,26 @@
+'use client';
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function HeaderButtons() {
+    const router = useRouter();
+    function login(){
+        router.push('../../auth/login')
+    }
+
+    function signup(){
+        router.push('../../auth/signup')
+    }
+    return (
+        <div className="flex justify-between items-center h-[40px] w-[464px]">
+            <Link href="" className="text-[#635C70]">Features</Link>
+            <Link href="" className="text-[#635C70] ">Pricing</Link>
+            <Link href="" className="text-[#635C70]">About</Link>
+            <Image src="/Assets/theme-icon.svg" alt='Change theme' width={25} height={25} />
+            <button type='button' onClick={login} className="w-[71px] h-[42px] rounded-md text-black bg-[#FFFFFF] flex items-center justify-center font-semibold">Log In</button>
+            <button type='button' onClick={signup} className="w-[107px] h-[42px] rounded-md text-white bg-[#5222D0] flex items-center justify-center font-semibold tracking-wideer">Get Started</button>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,33 @@
 import Image from "next/image"
+import HeaderButtons from './components/LandingPageNavBar';
 
 export default function indexPage() {
     return (
-        <div className="bg-black my-[96px] ">
-            <div className="UpperSection pl-[80px]">
-                <div className=" text mr-[80px] w-[644px]">
+        <div className="w-full flex flex-col bg-black">
+            <header className="bg-[#09090B] w-[100%] h-[70px] flex justify-between items-center px-[40px]">
+                <div className="flex mt-[15px] items-center">
+                    <Image src="/Assets/quiver-logo.png" alt='Quiver' width={50} height={50} className="mr-[10px]"/>
+                    <p className="text-[28px]">Quiver</p>
+                </div>
+                <HeaderButtons className="mt-[15px]"/>
+            </header>
+            <div className="UpperSection pl-[80px] pr-[80px] bg-black my-[96px] w-full flex items-center justify-between">
+                <div className=" text mr-[80px] w-[625px]">
                     <div className="w-[100%] h-[296px] pt-[27px]">
-                      <div><p className="text-white text-5xl font-bold">Aim.</p></div>
-                    <div><p className="text-[EC615B] text-5xl font-bold">Note.</p></div>
-                    <div><p className="text-[5222D0] text-5xl font-bold">Navigate.</p></div>
+                        <div><p className="text-white text-6xl font-bold">Aim.</p></div>
+                        <div><p className="text-[#EC615B] text-6xl font-bold ml-[18%]">Note.</p></div>
+                        <div><p className="text-[#5222D0] text-6xl font-bold ml-[39%]">Navigate.</p></div>
                     </div>
-                    <div className="w-[644px]">
-                        <p classname="w-[100%]">Our mission is to offer dynamic AI-based assessments to determine scholarship eligibility and empower students with valuable insights.</p>
+                    <div className="w-[100%] text-gray-700">
+                        <p className="w-[100%]">Our mission is to offer dynamic AI-based assessments to determine scholarship eligibility and empower students with valuable insights.</p>
                     </div>
                 </div>
                 <div className="Image">
-                    <Image></Image>
+                    <Image src="/Assets/landingPage.svg" width={596} height={446}></Image>
                 </div>
             </div>
             <div className="midSection">
-                
+
             </div>
             <div className="lowerSection">
 
